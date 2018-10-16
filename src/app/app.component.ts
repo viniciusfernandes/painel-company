5b718088dec9eecea206db38612289c4c2c292a9
vinicius <viniciussf@hotmail.com>
2018-10-16 07:36:48 -0300

    Inclusao de css dinamico nos botoes do painel sinalizando o botao pressionado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { Http } from '@angular/http';
 import { HttpClient } from '@angular/common/http';
@@ -13,8 +13,10 @@ export class AppComponent implements OnInit{
   descricaoTecnica:string;
   andares:number[][];
   mensagem:string='';
-  peso:string=null;
-  indice:string=null;
+  peso:number=null;
+  indice:number=null;
+  andarAtual:number;
+  
   constructor(private http:HttpClient){
     
   }
@@ -22,29 +24,30 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     let config:any;
     this.http.get('http://localhost:8080/elevador/config').subscribe(resp=> {
-    config = resp;
-    this.descricaoTecnica = config.descricao;
-    this.andares = [];
-    let tot =config.andarMax -config.andarMin ;
-    this.andares[0] = new Array<number>();
-    for (let i = 0,j=0; i <= tot; i++) {
-      if(i> 0 && i % 3 == 0){
-        if(this.andares[j] ){
-          this.andares[j].sort((n1,n2)=> n1 - n2);
-        }
-        j++;
-        this.andares[j] = new Array<number>();
-      } 
-      this.andares[j].push(i+config.andarMin);
-    }
+      config = resp;
+      this.descricaoTecnica = config.descricao;
+      this.andares = [];
+      let tot =config.andarMax -config.andarMin ;
+      this.andares[0] = new Array<number>();
+      
+      for (let i = 0,j=0; i <= tot; i++) {
+        if(i> 0 && i % 3 == 0){
+          if(this.andares[j] ){
+            this.andares[j].sort((n1,n2)=> n1 - n2);
+          }
+          j++;
+          this.andares[j] = new Array<number>();
+        } 
+        this.andares[j].push(i+config.andarMin);
+      }
 
-    let last:number[]=null;
-    
-    for(let k=0;k<this.andares.length/2;k++){
-     last = this.andares[this.andares.length-k-1];
-     this.andares[this.andares.length-k-1] = this.andares[k];
-     this.andares[k] = last;
-    }
+      let last:number[]=null;
+      
+      for(let k=0;k<this.andares.length/2;k++){
+      last = this.andares[this.andares.length-k-1];
+      this.andares[this.andares.length-k-1] = this.andares[k];
+      this.andares[k] = last;
+      }
 
    });
   
@@ -52,24 +55,22 @@ export class AppComponent implements OnInit{
 
   movimentar(andar:number){
     let m:any ; 
-    let peso:string;
-    let indice:string; 
-    if(this.indice === null || this.indice.trim().length <=0){
-      indice ='-1';
-    } else {
-      indice = this.indice;
-    }
-    if(this.peso === null || this.peso.trim().length <=0){
-      peso = '-1';
-    } else {
-      peso = this.peso;
-    }
-    this.http.get(`http://localhost:8080/elevador/movimentar/${indice}/${peso}/${andar}`).subscribe(
+   
+    if(this.indice === null){
+      this.indice =-1;
+    } 
+    
+    if(this.peso === null){
+      this.peso = -1;
+    } 
+    
+    this.http.get(`http://localhost:8080/elevador/movimentar/${this.indice}/${this.peso}/${andar}`).subscribe(
       resp=> {
         m = resp;
         this.mensagem = m.texto;
         this.indice=null;
         this.peso=null;
+        this.andarAtual = andar;
       },
       error=>{
         this.mensagem = `Alerta: ${error.error.texto}`;
@@ -78,4 +79,7 @@ export class AppComponent implements OnInit{
       });
   }
 
+  isAndarAtual(andar:number){
+    return this.andarAtual===andar;
+  }
 }
